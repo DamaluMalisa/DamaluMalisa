29872a16be24bfb577423dff81b2d3f33338c2e3
DamaluMalisa <damalumalisa@gmail.com>
2023-08-03 16:00:56 +0300

    Third commit

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable({})
-export class AuthService {}
+export class AuthService {
+  signup() {
+    return { msg: 'Hello i am signed up.' };
+  }
+
+  signin() {
+    return { msg: 'Hello i am signed in.' };
+  }
+}
